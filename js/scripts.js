7252e62cfe3401ba5831980417080b2b303c93d6
Mitchell Gantz <mgantz22@icloud.com>
2023-01-13 13:25:01 -0800

    Change form radio

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 function handleSelect(event) {
     event.preventDefault();
     const selection = document.getElementById("q1").value;
-    const selecttion = document.getElementById("q2").value;
-    const selection = document.getElementById("q3").value;
-    const selection = document.getElementById("q4").value;
-    const selection = document.getElementById("q5").value;
+
   }
 
   window.addEventListener("load", function() {
