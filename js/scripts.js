342e37d6c5e2438b267b21c1e10dcb821472be43
Mitchell Gantz <mgantz22@icloud.com>
2023-01-13 16:03:05 -0800

    brain broke

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
-function handleSelect(event) {
+function handleRadio(event) {
     event.preventDefault();
-    const selection = document.getElementById("q1").value;
-
+    const radioSelection = document.querySelector("input[name='q1']:checked").value;
+    //const radioSelection = document.querySelector("input[name='q2']:checked").value;
+    //const radioSelection = document.querySelector("input[name='q3']:checked").value;
+    //const radioSelection = document.querySelector("input[name='q4']:checked").value;
+    //const radioSelection = document.querySelector("input[name='q5']:checked").value;
   }
-
+  
   window.addEventListener("load", function() {
-    document.getElementById("select-form").addEventListener("submit", handleSelect);
+    document.getElementById("radio-form").addEventListener("submit", handleRadio);
+    if (radioSelection === "true") {
+        result = true;
+        document.getElementById("output").innerText = result;
+    }
   });
